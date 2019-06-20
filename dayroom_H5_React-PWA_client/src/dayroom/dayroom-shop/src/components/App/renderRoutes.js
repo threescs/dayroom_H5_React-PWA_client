829f3206b77ea9320dfcfffb0da60be9ecaa5a53
imiaomiao <aimiaomiao2016@163.com>
2019-06-20 14:20:12 +0800

    account information edit

--- dayroom_H5_React-PWA_client/src/dayroom/dayroom-shop/src/components/App/renderRoutes.js
+++ dayroom_H5_React-PWA_client/src/dayroom/dayroom-shop/src/components/App/renderRoutes.js
@@ -6,11 +6,15 @@ import CreateAccountPage from 'parentSrc/components/CreateAccountPage/index';
 // import Search from 'src/RootComponents/Search';
 // import Search from 'parentSrc/RootComponents/Search';
 import Account from 'src/RootComponents/Account';
+import AccountInfoForm from "src/RootComponents/AccountInfoForm";
+
 
 const renderRoutingError = props => <ErrorView {...props} />;
 const renderRoutes = () => (
     <Switch>
+        <Route exact path="/account/edit" component={AccountInfoForm} />
         <Route exact path="/account" component={Account} />
+
         {/* <Route exact path="/search.html" component={Search} /> */}
         <Route exact path="/create-account" component={CreateAccountPage} />
         <Route render={() => <Page>{renderRoutingError}</Page>} />
